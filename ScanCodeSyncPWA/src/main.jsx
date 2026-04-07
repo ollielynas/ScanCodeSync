e108521a19736c452e2078fa0b39b5e5d86d4741
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2026-04-07 14:00:11 +1200

    ok surely fixed now

--- ScanCodeSyncPWA/src/main.jsx
+++ ScanCodeSyncPWA/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { CookiesProvider } from "react-cookie";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {
+    console.log("App is ready to work offline - inputs will stay enabled!");
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
