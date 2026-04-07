ec1b7dc092baa3b92aa6eeecd7d751a4667dc1db
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2026-04-07 14:15:06 +1200

    forgot file

---
+++ ScanCodeSyncPWA/src/Pwa.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const InstallPWA = () => {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const onClick = (evt) => {
+    evt.preventDefault();
+    if (!supportsPWA) {
+      alert(
+        "This browser does not support installing progress web apps. Try Chrome or Edge",
+      );
+    }
+
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+
+  return (
+    <button
+      className="link-button"
+      id="setup_button"
+      aria-label="Install app"
+      title="Install app"
+      onClick={onClick}
+    >
+      Install
+    </button>
+  );
+};
+
+export default InstallPWA;
