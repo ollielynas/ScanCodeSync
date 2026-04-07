7f62bebe221ee4e0eecc3687c3d0ce154f3d788b
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2026-04-07 13:41:47 +1200

    hopefully the PWA is avalible offline now

--- ScanCodeSyncPWA/vite.config.js
+++ ScanCodeSyncPWA/vite.config.js
@@ -1,36 +1,41 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: '\u001a\u001aScan Code Sync',
-      short_name: '\u001aScan Code Sync',
-      description: 'An app that allows the syncing of metadata and media using simple scan codes and audio clips',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "Scan Code Sync",
+        short_name: "Scan Code Sync",
+        display: "standalone",
+        description:
+          "An app that allows the syncing of metadata and media using simple scan codes and audio clips",
+        theme_color: "#ffffff",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
