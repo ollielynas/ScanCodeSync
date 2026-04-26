bdd2a43831163362b0cb38af086a25aefc949c66
Ollie Lynas <49426014+ollielynas@users.noreply.github.com>
2026-04-26 16:34:24 +1200

    v0.0.16

--- ScanCodeSyncDesktop/src/main.rs
+++ ScanCodeSyncDesktop/src/main.rs
@@ -65,7 +65,7 @@ async fn main() {
     let mut state = State::default();
 
     #[cfg(target_os = "macos")]
-    setup_path();
+    command_pool::setup_path();
 
     #[cfg(target_os = "macos")]
     if matches!(prompt_install_brew(), Ok(true)) {
